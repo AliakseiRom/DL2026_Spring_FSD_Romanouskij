5bfbe21a02c60867c68fe180cc33e47d1fcca844
AliakseiRom <aliaksei.ramanouskij@gmail.com>
2026-03-14 16:28:49 +0300

    add log out button

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,17 +2,24 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 function App() {
+    const token = localStorage.getItem("token");
   return (
       <div>
         <h1>Mood Weather</h1>
-        <nav>
-          <Link to="/login">Login</Link> |
-          <Link to="/register">Register</Link> |
-          <Link to="/weather">Weather</Link> |
-          <Link to="/admin">Admin</Link>
-        </nav>
+          <nav>
+              <Link to="/login">Login</Link> |
+              <Link to="/register">Register</Link> |
+              <Link to="/weather">Weather</Link> |
+              <Link to="/admin">Admin</Link> |
+              {token && <button onClick={logout}>Logout</button>}
+          </nav>
       </div>
   );
 }
+
+const logout = () => {
+    localStorage.removeItem("token");
+    window.location.href = "/login";
+};
 
 export default App;
